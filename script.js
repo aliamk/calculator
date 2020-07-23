9b6bc89fff9ff600b9c6ea339da2b8f390dde076
aliamk <alia.azarian@gmail.com>
2020-07-24 00:47:54 +0100

    added functionality for the clear button and the disabled inputting multiple decimals

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const calculatorDisplay = document.querySelector('h1')
 const inputBtns = document.querySelectorAll('button')
 const clearBtn = document.getElementById('clear-btn')
 
+// Input numbers into the display screen
 function sendNumberValue(number) {
   // console.log(number)
   // if the current display value is 0, replace it; if not, add the number
@@ -10,13 +11,29 @@ function sendNumberValue(number) {
 }
 // console.log(inputBtns)
 
+function addDecimal() {
+  // If no decimal, add one (to stop user inputting multiple decimals)
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+
+  }
+}
+
 // Add Event Listeners for number, operators, decimal buttons
 inputBtns.forEach((inputBtn) => {
-  if (inputBtn.classList.length === 0) {   // the number buttons havent been assign classes in html
+  if (inputBtn.classList.length === 0) {   // the number buttons havent been assigned classes in html
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value)) // return the number stored in the buttons' value
   } else if (inputBtn.classList.contains('operator')) { // if a button has a class="operator" in index.html
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value)) // return the operator symbol
   } else if (inputBtn.classList.contains('decimal')) { // if a button has a class="decimal" symbol
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value)) // 
+    inputBtn.addEventListener('click', () => addDecimal()) // 
   }
 })
+
+// Reset display
+function resetAll() {
+  calculatorDisplay.textContent = '0'
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll)
